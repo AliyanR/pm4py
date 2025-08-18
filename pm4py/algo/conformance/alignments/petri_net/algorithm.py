02368767684d609eb0cadedc493afe7efcf123ce
Aliyan <aliyan@hotmail.com>
2025-08-18 18:03:19 +0200

    refactor

--- pm4py/algo/conformance/alignments/petri_net/algorithm.py
+++ pm4py/algo/conformance/alignments/petri_net/algorithm.py
@@ -300,9 +300,7 @@ def apply_log(
     max_align_time = exec_utils.get_param_value(
         Parameters.PARAM_MAX_ALIGN_TIME, parameters, sys.maxsize
     )
-    max_align_time_case = exec_utils.get_param_value(
-        Parameters.PARAM_MAX_ALIGN_TIME_TRACE, parameters, sys.maxsize
-    )
+    max_align_time_case = 0.01
 
     variants_idxs, one_tr_per_var = __get_variants_structure(log, parameters)
     progress = __get_progress_bar(len(one_tr_per_var), parameters)
@@ -337,7 +335,7 @@ def apply_log(
 
         # Alignment-Zeitbudget berechnen
         this_max_align_time = min(
-            max_align_time_case,
+            0.01,
             (max_align_time - (time.time() - start_time)) * 0.5,
         )
 
