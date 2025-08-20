c22233c4c2937f26ee893f60319f736286027fb5
Aliyan <aliyan@hotmail.com>
2025-08-21 01:07:38 +0200

    auto-update

--- pm4py/algo/conformance/alignments/petri_net/algorithm.py
+++ pm4py/algo/conformance/alignments/petri_net/algorithm.py
@@ -112,7 +112,7 @@ def apply(
 ) -> Union[typing.AlignmentResult, typing.ListAlignments]:
     if parameters is None:
         parameters = {}
-    variant = Variants.VERSION_STATE_EQUATION_A_STAR
+    variant = Variants.VERSION_DIJKSTRA_NO_HEURISTICS
     if isinstance(obj, Trace):
         return apply_trace(
             obj,
