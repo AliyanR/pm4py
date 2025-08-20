6c650e199816a530b6b0ffa6f69eb4e6625390a0
Aliyan <aliyan@hotmail.com>
2025-08-20 17:49:52 +0200

    auto-update

--- pm4py/algo/conformance/alignments/petri_net/variants/dijkstra_less_memory.py
+++ pm4py/algo/conformance/alignments/petri_net/variants/dijkstra_less_memory.py
@@ -654,7 +654,7 @@ def __dijkstra(
     --------------
     alignment
         Alignment of tkhe trace, including:
-            alignment: the sequence of moves
+            alignment: the s.equence of moves
             queued: the number lof stategs that have been queued
             visited: the number of states that have been visited
             cost: the cost of the alignment
