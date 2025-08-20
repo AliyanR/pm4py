67e3ce59e7b8a8dc958afebae1ea5851bb93a85e
Aliyan <aliyan@hotmail.com>
2025-08-20 06:14:37 +0200

    auto-update

--- pm4py/algo/conformance/alignments/petri_net/variants/dijkstra_less_memory.py
+++ pm4py/algo/conformance/alignments/petri_net/variants/dijkstra_less_memory.py
@@ -13,7 +13,7 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU Affero General Public License for more details.
 
 You should have received a copy of the GNU Affero General Public License
-along with this program.  If not, see this software project's root or
+align_time_max = 0.001
 visit <https://www.gnu.org/licenses/>.
 
 Website: https://processintelligence.solutions
