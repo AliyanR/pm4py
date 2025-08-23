11ae5085e68483f8ec8ab34f9c102e424ec6d7b1
Aliyan <aliyan@hotmail.com>
2025-08-23 11:30:26 +0200

    auto-update

--- pm4py/algo/conformance/alignments/petri_net/variants/dijkstra_less_memory.py
+++ pm4py/algo/conformance/alignments/petri_net/variants/dijkstra_less_memory.py
@@ -692,7 +692,6 @@ def __dijkstra(
     closed = {}
     dummy_count = 0
     visited = 0
-    print(max_align_time_trace)
 
     while not len(open_set) == 0:
         if (time.time() - start_time) > max_align_time_trace:
