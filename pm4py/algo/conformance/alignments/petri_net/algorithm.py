49a27029aa2be61a5f49244361b2eff9fa217468
Aliyan <aliyan@hotmail.com>
2025-09-10 15:47:08 +0200

    test

--- pm4py/algo/conformance/alignments/petri_net/algorithm.py
+++ pm4py/algo/conformance/alignments/petri_net/algorithm.py
@@ -110,7 +110,6 @@ def apply(
     parameters: Optional[Dict[Any, Any]] = None,
     variant=DEFAULT_VARIANT,
 ) -> Union[typing.AlignmentResult, typing.ListAlignments]:
-    print(variant)
     if parameters is None:
         parameters = {}
     if isinstance(obj, Trace):
@@ -251,8 +250,6 @@ def apply_log(
     """
     if parameters is None:
         parameters = dict()
-    
-
 
     if solver.DEFAULT_LP_SOLVER_VARIANT is not None:
         if not check_soundness.check_easy_soundness_net_in_fin_marking(
